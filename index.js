c65fe130793f3987d85d429f3a07cf155b33860c
CrazyStudiosYT <ijeomahorjia@gmail.com>
2021-07-09 19:49:31 +0100

    *

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ client.on('message',  message => {
                     message.delete({timeout: 1})
                     client.channels.cache.get("697426937047678997").send(`Please welcome <@${message.author.id}> to the server!`);
                     discord_terminal("**" + message.author.tag + "** has just entered the Server!");
-                }
+                } else message.delete({timeout: 1});
             }
         } else if (message.channel.id === "696010902196977784") {
             if (message.content == "Gamer" || message.content == "gamer") {
