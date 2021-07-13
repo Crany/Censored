06671b66df473501038626a005a80416572d9025
CrazyStudiosYT <ijeomahorjia@gmail.com>
2021-07-13 09:11:38 +0100

    *

--- commands/ping.js
+++ commands/ping.js
@@ -5,8 +5,6 @@ module.exports = {
     name: 'ping',
     description: 'this is a ping command',
     async execute (message, args) {
-        let msg = await message.channel.send("Pong!");
-        var ping = Math.round(client.ws.ping)
-        msg.edit(`Pong \`${ping}\``)
+        
     }
 }
--- commands/roles/giverole.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "giverole",
-    description: "Allows Deputy Legends and higher to give roles",
-    execute(message, args) {
-        // code here
-    }
-}
