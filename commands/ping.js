a8bf3795bee4fa86917aef262775e7898c782443
CrazyStudiosYT <ijeomahorjia@gmail.com>
2021-07-13 09:01:27 +0100

    *

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ const client = discord.Client
 module.exports = {
     name: 'ping',
     description: 'this is a ping command',
-    execute (message, args) {
+    async execute (message, args) {
         let msg = await message.channel.send("Pong!");
         var ping = Math.round(client.ws.ping)
         msg.edit(`Pong \`${ping}\``)
