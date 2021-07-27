2e87f048543a5613243ee953624d1e0d7aa834aa
CrazyStudiosYT <ijeomahorjia@gmail.com>
2021-07-27 15:47:52 +0100

    *

--- index.js
+++ index.js
@@ -504,61 +504,65 @@ client.on('message', async (message) => {
 
                     let newargs = args.slice(2).join(" ");
 
-                    if (newargs == (null || "")) {
-                        console.log(member.user.username)
-                        if (member == null) {
-                            if (message.member.roles.cache.has(AdminRole)) {
-                                message.reply("Sorry, I'm not allowed to complete this action.");
-                            } else if (message.member.roles.cache.has(ModsRoles)) {
-                                message.member.setNickname("[Mod] " + message.member.user.username)
-                            } else if (message.member.roles.cache.has(StaffRole)) {
-                                message.member.setNickname("[Staff] " + message.member.user.username)
-                            } else if (message.member.roles.cache.has(AdancedRole)) {
-                                message.member.setNickname("[Advanced] " + message.member.user.username)
-                            } else {
-                                message.member.setNickname(member.user.username)
+                    if (member == member.user.bot) {
+                        if (newargs == (null || "")) {
+                            console.log(member.user.username)
+                            if (member == null) {
+                                if (message.member.roles.cache.has(AdminRole)) {
+                                    message.reply("Sorry, I'm not allowed to complete this action.");
+                                } else if (message.member.roles.cache.has(ModsRoles)) {
+                                    message.member.setNickname("[Mod] " + message.member.user.username)
+                                } else if (message.member.roles.cache.has(StaffRole)) {
+                                    message.member.setNickname("[Staff] " + message.member.user.username)
+                                } else if (message.member.roles.cache.has(AdancedRole)) {
+                                    message.member.setNickname("[Advanced] " + message.member.user.username)
+                                } else {
+                                    message.member.setNickname(member.user.username)
+                                }
+                            } else if (member != null) {
+                                if (member.roles.cache.has(AdminRole)) {
+                                    message.reply("Sorry, I'm not allowed to complete this action.")
+                                } else if (member.roles.cache.has(ModsRoles)) {
+                                    member.setNickname("[Mod] " + member.user.username)
+                                } else if (member.roles.cache.has(StaffRole)) {
+                                    member.setNickname("[Staff] " + member.user.username)
+                                } else if (member.roles.cache.has(AdancedRole)) {
+                                    member.setNickname("[Advanced] " + member.user.username)
+                                } else {
+                                    member.setNickname(member.user.tag)
+                                }
                             }
-                        } else if (member != null) {
-                            if (member.roles.cache.has(AdminRole)) {
-                                message.reply("Sorry, I'm not allowed to complete this action.")
-                            } else if (member.roles.cache.has(ModsRoles)) {
-                                member.setNickname("[Mod] " + member.user.username)
-                            } else if (member.roles.cache.has(StaffRole)) {
-                                member.setNickname("[Staff] " + member.user.username)
-                            } else if (member.roles.cache.has(AdancedRole)) {
-                                member.setNickname("[Advanced] " + member.user.username)
-                            } else {
-                                member.setNickname(member.user.tag)
-                            }
-                        }
-                    } else if (newargs != "") {
+                        } else if (newargs != "") {
 
-                        if (member == null) {
-                            if (message.member.roles.cache.has(AdminRole)) {
-                                message.reply("Sorry, I'm not allowed to complete this action.");
-                            } else if (message.member.roles.cache.has(ModsRoles)) {
-                                message.member.setNickname("[Mod] " + newargs)
-                            } else if (message.member.roles.cache.has(StaffRole)) {
-                                message.member.setNickname("[Staff] " + newargs)
-                            } else if (message.member.roles.cache.has(AdancedRole)) {
-                                message.member.setNickname("[Advanced] " + newargs)
-                            } else {
-                                message.member.setNickname(newargs)
-                            }
-                        } else if (member != null) {
-                            if (member.roles.cache.has(AdminRole)) {
-                                message.reply("Sorry, I'm not allowed to complete this action.")
-                            } else if (member.roles.cache.has(ModsRoles)) {
-                                member.setNickname("[Mod] " + newargs)
-                            } else if (member.roles.cache.has(StaffRole)) {
-                                member.setNickname("[Staff] " + newargs)
-                            } else if (member.roles.cache.has(AdancedRole)) {
-                                member.setNickname("[Advanced] " + newargs)
-                            } else {
-                                member.setNickname(newargs)
+                            if (member == null) {
+                                if (message.member.roles.cache.has(AdminRole)) {
+                                    message.reply("Sorry, I'm not allowed to complete this action.");
+                                } else if (message.member.roles.cache.has(ModsRoles)) {
+                                    message.member.setNickname("[Mod] " + newargs)
+                                } else if (message.member.roles.cache.has(StaffRole)) {
+                                    message.member.setNickname("[Staff] " + newargs)
+                                } else if (message.member.roles.cache.has(AdancedRole)) {
+                                    message.member.setNickname("[Advanced] " + newargs)
+                                } else {
+                                    message.member.setNickname(newargs)
+                                }
+                            } else if (member != null) {
+                                if (member.roles.cache.has(AdminRole)) {
+                                    message.reply("Sorry, I'm not allowed to complete this action.")
+                                } else if (member.roles.cache.has(ModsRoles)) {
+                                    member.setNickname("[Mod] " + newargs)
+                                } else if (member.roles.cache.has(StaffRole)) {
+                                    member.setNickname("[Staff] " + newargs)
+                                } else if (member.roles.cache.has(AdancedRole)) {
+                                    member.setNickname("[Advanced] " + newargs)
+                                } else {
+                                    member.setNickname(newargs)
+                                }
                             }
                         }
                     }
+
+                    
 
                     
                 }
