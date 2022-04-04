86ab190e4e23acd36df3fb944ecdabb46e2dde5a
CranyBrainly <ijeomahorjia@gmail.com>
2022-04-04 17:36:26 +0100

    *

--- models/prefix.js
+++ models/prefix.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose') // Optional
 const prefixSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     prefix: String,
-    userID: String,
 })
 
 module.exports = mongoose.model("Prefix", prefixSchema)
